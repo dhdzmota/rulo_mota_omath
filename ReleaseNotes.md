9876bfbdcfa8d9150e7a9ac0aaf3a602940aac61
Omath <omar@synx.co>
2022-01-26 21:02:35 -0600

    First commit with cookie-cutter structure

---
+++ ReleaseNotes.md
@@ -0,0 +1,10 @@
+# Release Notes
+
+## Version 1.0.0
+  - First commit.
+### BugFixes
+  - No bugfixes
+### HotFixes
+  - There are not hot fixes
+### Notable Info
+  - First version
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='1.0.0',
+    description='Data Mexico',
+    author='rulo_mota_omath',
+    license='MIT',
+)
